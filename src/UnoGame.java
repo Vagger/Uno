f8bb521df10c6a9cdc97f5291efbc123ae3f4372
vaheek <vahekarapetyan47@gmail.com>
2025-11-17 12:49:37 +0400

    Minor fix

--- src/UnoGame.java
+++ src/UnoGame.java
@@ -60,6 +60,7 @@ public class UnoGame {
             }
             if (currentCard.isReverse()) {
                 clockwiseOrder = !clockwiseOrder;
+                continue;
             }
 
             if (currentPlayer.hasPlayableCard(currentCard)) {
